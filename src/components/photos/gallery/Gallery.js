ccb0207510f5a4a4ba4d0ea9660bccc4619e1626
Santiago Rochi <santiagorochi9@gmail.com>
2021-11-10 05:34:40 -0300

    test

---
+++ src/components/photos/gallery/Gallery.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+
+export const Gallery=({data})=>{
+       console.log('a',data)
+       return(
+              <div>
+                     <html lang="en">
+    
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Arcade</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=latin-ext" rel="stylesheet"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="reset.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>
+    
+    <div class="container">
+        
+
+       <div class="item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/placeimg01.jpg"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/34435/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/34088/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/900105/pexels-photo-900105.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/276374/pexels-photo-276374.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/688890/pexels-photo-688890.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+  
+        <div class="item">
+            <img src="https://images.pexels.com/photos/220836/pexels-photo-220836.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+        
+        <div class="item">
+            <img src="https://images.pexels.com/photos/145707/pexels-photo-145707.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+    </div>
+</body>
+</html>
+</div>
+       )
+}
+
+
+Gallery.propTypes = {
+    data: PropTypes.object
+}
