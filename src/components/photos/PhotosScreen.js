8301d2ad39ddcf45f894691b060cd5bf23901137
Santiago Rochi <santiagorochi9@gmail.com>
2021-11-09 21:52:29 -0300

    pag

--- src/components/photos/PhotosScreen.js
+++ src/components/photos/PhotosScreen.js
@@ -1,11 +1,72 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import { Pagination } from '../ui/Pagination';
 
 export const PhotosScreen=()=>{
 
        return(
               <div>
                      <Navbar/>
-              </div>
+                     <Pagination/>
+                     <html lang="en">
+    
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Arcade</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=latin-ext" rel="stylesheet"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="reset.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>
+    
+    <div class="container">
+        
+        <div class="item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/placeimg01.jpg"/>
+        </div>
+
+       <div class="item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/placeimg01.jpg"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/34435/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/34088/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/900105/pexels-photo-900105.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/276374/pexels-photo-276374.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/688890/pexels-photo-688890.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+
+        <div class="item">
+            <img src="https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+  
+        <div class="item">
+            <img src="https://images.pexels.com/photos/220836/pexels-photo-220836.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+        
+        <div class="item">
+            <img src="https://images.pexels.com/photos/145707/pexels-photo-145707.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+        </div>
+    </div>
+</body>
+</html>
+<Pagination/>
+</div>
        )
 }
